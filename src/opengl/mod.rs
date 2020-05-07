dd540a1824ec60b189e2fe4552f797a5a286c1bf
Srimanta Barua <srimanta.barua1@gmail.com>
2020-05-07 13:42:02 +0530

    More opengl functions we'll need

--- src/opengl/mod.rs
+++ src/opengl/mod.rs
@@ -42,3 +42,42 @@ pub(crate) fn gl_clear_color(color: Color) {
 pub(crate) fn gl_clear() {
     unsafe { gl::Clear(gl::COLOR_BUFFER_BIT | gl::STENCIL_BUFFER_BIT) };
 }
+
+// Enable/disable stencil tests
+pub(super) fn gl_set_stencil_test(val: bool) {
+    if val {
+        unsafe {
+            gl::Enable(gl::STENCIL_TEST);
+            gl::StencilOp(gl::ZERO, gl::ZERO, gl::REPLACE);
+        }
+    } else {
+        unsafe {
+            gl::Disable(gl::STENCIL_TEST);
+        }
+    }
+}
+
+// Enable stencil modification
+pub(super) fn gl_set_stencil_writing() {
+    unsafe {
+        gl::StencilFunc(gl::ALWAYS, 1, 0xff);
+        gl::StencilMask(0xff);
+    }
+}
+
+// Disable stencil modification, and read from stencil
+pub(super) fn gl_set_stencil_reading() {
+    unsafe {
+        gl::StencilFunc(gl::EQUAL, 1, 0xff);
+        gl::StencilMask(0x00);
+    }
+}
+
+// Clear stencil buffer
+pub(super) fn gl_clear_stencil() {
+    unsafe {
+        gl::StencilMask(0xff);
+        gl::Clear(gl::STENCIL_BUFFER_BIT);
+        gl::StencilMask(0x00);
+    }
+}
