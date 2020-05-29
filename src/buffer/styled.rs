a83c9c422c79a4261cce99d1f84a1d5f76f2422d
Srimanta Barua <srimanta.barua1@gmail.com>
2020-05-30 00:08:05 +0530

    Optimization, but a lot of code duplication :/

--- src/buffer/styled.rs
+++ src/buffer/styled.rs
@@ -1,5 +1,6 @@
 // (C) 2020 Srimanta Barua <srimanta.barua1@gmail.com>
 
+use std::cmp::max;
 use std::ops::Range;
 
 use crate::style::{Color, TextStyle};
@@ -41,63 +42,119 @@ impl StyledText {
 
     // TODO: Merge adjacent if required
     fn set_style(&mut self, range: Range<usize>, style: TextStyle) {
-        let mut i = match self.styles.binary_search_by_key(&range.start, |x| x.0) {
+        let i = match self.styles.binary_search_by_key(&range.start, |x| x.0) {
             Ok(i) => i + 1,
             Err(i) => i,
         };
         assert!(i < self.styles.len());
-        if range.start == 0 || (i > 0 && range.start == self.styles[i - 1].0) {
-            self.styles.insert(i, (range.end, style));
-            i += 1;
-        } else {
-            self.styles.insert(i, (range.start, self.styles[i].1));
-            self.styles.insert(i + 1, (range.end, style));
-            i += 2;
+        while i + 1 < self.styles.len() && self.styles[i + 1].0 <= range.end {
+            self.styles.remove(i + 1);
         }
-        // Remove everything after this that is completely covered
-        while i < self.styles.len() && self.styles[i].0 <= range.end {
+        if (range.start == 0 || (i > 0 && range.start == self.styles[i - 1].0))
+            && self.styles[i].0 <= range.end
+        {
             self.styles.remove(i);
+        }
+        if i < self.styles.len() && style == self.styles[i].1 {
+            self.styles[i].0 = max(range.end, self.styles[i].0);
+            // Merge with previous if required, and return
+            if i > 0 && self.styles[i - 1].1 == self.styles[i].1 {
+                self.styles[i - 1].0 = self.styles[i].0;
+                self.styles.remove(i);
+            }
+            return;
+        }
+        if range.start == 0 || (i > 0 && range.start == self.styles[i - 1].0) {
+            if i > 0 && self.styles[i - 1].1 == style {
+                self.styles[i - 1].0 = range.end;
+            } else {
+                self.styles.insert(i, (range.end, style));
+            }
+            return;
+        }
+        self.styles.insert(i, (range.start, self.styles[i].1));
+        if self.styles[i + 1].0 <= range.end {
+            self.styles[i + 1] = (range.end, style);
+        } else {
+            self.styles.insert(i + 1, (range.end, style));
         }
     }
 
     fn set_color(&mut self, range: Range<usize>, color: Color) {
-        assert!(self.colors.len() > 0 && range.start < self.colors[self.colors.len() - 1].0);
-        let mut i = match self.colors.binary_search_by_key(&range.start, |x| x.0) {
+        let i = match self.colors.binary_search_by_key(&range.start, |x| x.0) {
             Ok(i) => i + 1,
             Err(i) => i,
         };
         assert!(i < self.colors.len());
-        if range.start == 0 || (i > 0 && range.start == self.colors[i - 1].0) {
-            self.colors.insert(i, (range.end, color));
-            i += 1;
-        } else {
-            self.colors.insert(i, (range.start, self.colors[i].1));
-            self.colors.insert(i + 1, (range.end, color));
-            i += 2;
+        while i + 1 < self.colors.len() && self.colors[i + 1].0 <= range.end {
+            self.colors.remove(i + 1);
         }
-        // Remove everything after this that is completely covered
-        while i < self.colors.len() && self.colors[i].0 <= range.end {
+        if (range.start == 0 || (i > 0 && range.start == self.colors[i - 1].0))
+            && self.colors[i].0 <= range.end
+        {
             self.colors.remove(i);
+        }
+        if i < self.colors.len() && color == self.colors[i].1 {
+            self.colors[i].0 = max(range.end, self.colors[i].0);
+            // Merge with previous if required, and return
+            if i > 0 && self.colors[i - 1].1 == self.colors[i].1 {
+                self.colors[i - 1].0 = self.colors[i].0;
+                self.colors.remove(i);
+            }
+            return;
+        }
+        if range.start == 0 || (i > 0 && range.start == self.colors[i - 1].0) {
+            if i > 0 && self.colors[i - 1].1 == color {
+                self.colors[i - 1].0 = range.end;
+            } else {
+                self.colors.insert(i, (range.end, color));
+            }
+            return;
+        }
+        self.colors.insert(i, (range.start, self.colors[i].1));
+        if self.colors[i + 1].0 <= range.end {
+            self.colors[i + 1] = (range.end, color);
+        } else {
+            self.colors.insert(i + 1, (range.end, color));
         }
     }
 
     fn set_under(&mut self, range: Range<usize>, under: Option<Color>) {
-        let mut i = match self.unders.binary_search_by_key(&range.start, |x| x.0) {
+        let i = match self.unders.binary_search_by_key(&range.start, |x| x.0) {
             Ok(i) => i + 1,
             Err(i) => i,
         };
         assert!(i < self.unders.len());
-        if range.start == 0 || (i > 0 && range.start == self.unders[i - 1].0) {
-            self.unders.insert(i, (range.end, under));
-            i += 1;
-        } else {
-            self.unders.insert(i, (range.start, self.unders[i].1));
-            self.unders.insert(i + 1, (range.end, under));
-            i += 2;
+        while i + 1 < self.unders.len() && self.unders[i + 1].0 <= range.end {
+            self.unders.remove(i + 1);
         }
-        // Remove everything after this that is completely covered
-        while i < self.unders.len() && self.unders[i].0 <= range.end {
+        if (range.start == 0 || (i > 0 && range.start == self.unders[i - 1].0))
+            && self.unders[i].0 <= range.end
+        {
             self.unders.remove(i);
+        }
+        if i < self.unders.len() && under == self.unders[i].1 {
+            self.unders[i].0 = max(range.end, self.unders[i].0);
+            // Merge with previous if required, and return
+            if i > 0 && self.unders[i - 1].1 == self.unders[i].1 {
+                self.unders[i - 1].0 = self.unders[i].0;
+                self.unders.remove(i);
+            }
+            return;
+        }
+        if range.start == 0 || (i > 0 && range.start == self.unders[i - 1].0) {
+            if i > 0 && self.unders[i - 1].1 == under {
+                self.unders[i - 1].0 = range.end;
+            } else {
+                self.unders.insert(i, (range.end, under));
+            }
+            return;
+        }
+        self.unders.insert(i, (range.start, self.unders[i].1));
+        if self.unders[i + 1].0 <= range.end {
+            self.unders[i + 1] = (range.end, under);
+        } else {
+            self.unders.insert(i + 1, (range.end, under));
         }
     }
 }
@@ -133,7 +190,7 @@ mod tests {
                 (10, Color::new(0, 0, 0, 0))
             ]
         );
-        assert_eq!(styled.unders, vec![(1, None), (4, None), (10, None)]);
+        assert_eq!(styled.unders, vec![(10, None)]);
         assert_eq!(
             styled.styles,
             vec![
@@ -161,21 +218,13 @@ mod tests {
         );
         assert_eq!(
             styled.unders,
-            vec![
-                (1, None),
-                (4, None),
-                (5, None),
-                (8, Some(Color::new(0, 0, 0, 0xff))),
-                (10, None)
-            ]
+            vec![(5, None), (8, Some(Color::new(0, 0, 0, 0xff))), (10, None)]
         );
         assert_eq!(
             styled.styles,
             vec![
                 (1, TextStyle::default()),
                 (4, TextStyle::new(TextWeight::Bold, TextSlant::Italic)),
-                (5, TextStyle::default()),
-                (8, TextStyle::default()),
                 (10, TextStyle::default())
             ]
         );
@@ -190,8 +239,6 @@ mod tests {
             styled.colors,
             vec![
                 (1, Color::new(0, 0, 0, 0)),
-                (4, Color::new(0xff, 0, 0, 0)),
-                (5, Color::new(0xff, 0, 0, 0)),
                 (8, Color::new(0xff, 0, 0, 0)),
                 (10, Color::new(0, 0, 0, 0))
             ]
@@ -199,7 +246,6 @@ mod tests {
         assert_eq!(
             styled.unders,
             vec![
-                (1, None),
                 (4, None),
                 (5, Some(Color::new(0xff, 0, 0, 0))),
                 (8, Some(Color::new(0, 0, 0, 0xff))),
@@ -212,7 +258,6 @@ mod tests {
                 (1, TextStyle::default()),
                 (4, TextStyle::new(TextWeight::Bold, TextSlant::Italic)),
                 (5, TextStyle::new(TextWeight::Light, TextSlant::Oblique)),
-                (8, TextStyle::default()),
                 (10, TextStyle::default())
             ]
         );
@@ -227,21 +272,13 @@ mod tests {
             styled.colors,
             vec![
                 (1, Color::new(0, 0, 0, 0)),
-                (3, Color::new(0xff, 0, 0, 0)),
-                (7, Color::new(0xff, 0, 0, 0)),
                 (8, Color::new(0xff, 0, 0, 0)),
                 (10, Color::new(0, 0, 0, 0))
             ]
         );
         assert_eq!(
             styled.unders,
-            vec![
-                (1, None),
-                (3, None),
-                (7, None),
-                (8, Some(Color::new(0, 0, 0, 0xff))),
-                (10, None)
-            ]
+            vec![(7, None), (8, Some(Color::new(0, 0, 0, 0xff))), (10, None)]
         );
         assert_eq!(
             styled.styles,
@@ -249,7 +286,6 @@ mod tests {
                 (1, TextStyle::default()),
                 (3, TextStyle::new(TextWeight::Bold, TextSlant::Italic)),
                 (7, TextStyle::new(TextWeight::Light, TextSlant::Oblique)),
-                (8, TextStyle::default()),
                 (10, TextStyle::default())
             ]
         );
