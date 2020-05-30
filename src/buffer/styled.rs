20e17b785f7d47e05c835a07e2a58cb84b71c320
Srimanta Barua <srimanta.barua1@gmail.com>
2020-05-30 14:48:02 +0530

    Cut down on code duplication for StyledText

--- src/buffer/styled.rs
+++ src/buffer/styled.rs
@@ -27,7 +27,6 @@ impl StyledText {
         }
     }
 
-    // TODO: Cut down on code duplication
     pub(super) fn set(
         &mut self,
         range: Range<usize>,
@@ -35,127 +34,49 @@ impl StyledText {
         color: Color,
         under: Option<Color>,
     ) {
-        self.set_style(range.clone(), style);
-        self.set_color(range.clone(), color);
-        self.set_under(range, under);
+        set(&mut self.styles, range.clone(), style);
+        set(&mut self.colors, range.clone(), color);
+        set(&mut self.unders, range, under);
     }
+}
 
-    // TODO: Merge adjacent if required
-    fn set_style(&mut self, range: Range<usize>, style: TextStyle) {
-        let i = match self.styles.binary_search_by_key(&range.start, |x| x.0) {
-            Ok(i) => i + 1,
-            Err(i) => i,
-        };
-        assert!(i < self.styles.len());
-        while i + 1 < self.styles.len() && self.styles[i + 1].0 <= range.end {
-            self.styles.remove(i + 1);
-        }
-        if (range.start == 0 || (i > 0 && range.start == self.styles[i - 1].0))
-            && self.styles[i].0 <= range.end
-        {
-            self.styles.remove(i);
-        }
-        if i < self.styles.len() && style == self.styles[i].1 {
-            self.styles[i].0 = max(range.end, self.styles[i].0);
-            // Merge with previous if required, and return
-            if i > 0 && self.styles[i - 1].1 == self.styles[i].1 {
-                self.styles[i - 1].0 = self.styles[i].0;
-                self.styles.remove(i);
-            }
-            return;
-        }
-        if range.start == 0 || (i > 0 && range.start == self.styles[i - 1].0) {
-            if i > 0 && self.styles[i - 1].1 == style {
-                self.styles[i - 1].0 = range.end;
-            } else {
-                self.styles.insert(i, (range.end, style));
-            }
-            return;
-        }
-        self.styles.insert(i, (range.start, self.styles[i].1));
-        if self.styles[i + 1].0 <= range.end {
-            self.styles[i + 1] = (range.end, style);
-        } else {
-            self.styles.insert(i + 1, (range.end, style));
-        }
+fn set<T>(vec: &mut Vec<(usize, T)>, range: Range<usize>, val: T)
+where
+    T: Copy + Eq,
+{
+    let i = match vec.binary_search_by_key(&range.start, |x| x.0) {
+        Ok(i) => i + 1,
+        Err(i) => i,
+    };
+    assert!(i < vec.len());
+    while i + 1 < vec.len() && vec[i + 1].0 <= range.end {
+        vec.remove(i + 1);
     }
-
-    fn set_color(&mut self, range: Range<usize>, color: Color) {
-        let i = match self.colors.binary_search_by_key(&range.start, |x| x.0) {
-            Ok(i) => i + 1,
-            Err(i) => i,
-        };
-        assert!(i < self.colors.len());
-        while i + 1 < self.colors.len() && self.colors[i + 1].0 <= range.end {
-            self.colors.remove(i + 1);
-        }
-        if (range.start == 0 || (i > 0 && range.start == self.colors[i - 1].0))
-            && self.colors[i].0 <= range.end
-        {
-            self.colors.remove(i);
-        }
-        if i < self.colors.len() && color == self.colors[i].1 {
-            self.colors[i].0 = max(range.end, self.colors[i].0);
-            // Merge with previous if required, and return
-            if i > 0 && self.colors[i - 1].1 == self.colors[i].1 {
-                self.colors[i - 1].0 = self.colors[i].0;
-                self.colors.remove(i);
-            }
-            return;
-        }
-        if range.start == 0 || (i > 0 && range.start == self.colors[i - 1].0) {
-            if i > 0 && self.colors[i - 1].1 == color {
-                self.colors[i - 1].0 = range.end;
-            } else {
-                self.colors.insert(i, (range.end, color));
-            }
-            return;
-        }
-        self.colors.insert(i, (range.start, self.colors[i].1));
-        if self.colors[i + 1].0 <= range.end {
-            self.colors[i + 1] = (range.end, color);
-        } else {
-            self.colors.insert(i + 1, (range.end, color));
-        }
+    if (range.start == 0 || (i > 0 && range.start == vec[i - 1].0)) && vec[i].0 <= range.end {
+        vec.remove(i);
     }
-
-    fn set_under(&mut self, range: Range<usize>, under: Option<Color>) {
-        let i = match self.unders.binary_search_by_key(&range.start, |x| x.0) {
-            Ok(i) => i + 1,
-            Err(i) => i,
-        };
-        assert!(i < self.unders.len());
-        while i + 1 < self.unders.len() && self.unders[i + 1].0 <= range.end {
-            self.unders.remove(i + 1);
+    if i < vec.len() && val == vec[i].1 {
+        vec[i].0 = max(range.end, vec[i].0);
+        // Merge with previous if required, and return
+        if i > 0 && vec[i - 1].1 == vec[i].1 {
+            vec[i - 1].0 = vec[i].0;
+            vec.remove(i);
         }
-        if (range.start == 0 || (i > 0 && range.start == self.unders[i - 1].0))
-            && self.unders[i].0 <= range.end
-        {
-            self.unders.remove(i);
-        }
-        if i < self.unders.len() && under == self.unders[i].1 {
-            self.unders[i].0 = max(range.end, self.unders[i].0);
-            // Merge with previous if required, and return
-            if i > 0 && self.unders[i - 1].1 == self.unders[i].1 {
-                self.unders[i - 1].0 = self.unders[i].0;
-                self.unders.remove(i);
-            }
-            return;
-        }
-        if range.start == 0 || (i > 0 && range.start == self.unders[i - 1].0) {
-            if i > 0 && self.unders[i - 1].1 == under {
-                self.unders[i - 1].0 = range.end;
-            } else {
-                self.unders.insert(i, (range.end, under));
-            }
-            return;
-        }
-        self.unders.insert(i, (range.start, self.unders[i].1));
-        if self.unders[i + 1].0 <= range.end {
-            self.unders[i + 1] = (range.end, under);
+        return;
+    }
+    if range.start == 0 || (i > 0 && range.start == vec[i - 1].0) {
+        if i > 0 && vec[i - 1].1 == val {
+            vec[i - 1].0 = range.end;
         } else {
-            self.unders.insert(i + 1, (range.end, under));
+            vec.insert(i, (range.end, val));
         }
+        return;
+    }
+    vec.insert(i, (range.start, vec[i].1));
+    if vec[i + 1].0 <= range.end {
+        vec[i + 1] = (range.end, val);
+    } else {
+        vec.insert(i + 1, (range.end, val));
     }
 }
 
